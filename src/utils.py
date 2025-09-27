b61b4566100d5e4b5aa317fcd12a8aee7cbb6ece
brightsnow02 <thoratharshavardhan1@gmail.com>
2025-09-28 00:16:50 +0530

    model trainer

--- src/utils.py
+++ src/utils.py
@@ -1,11 +1,15 @@
-# This code should be inside src/utils.py
 import os
+import sys
 import dill
+import numpy as np
+import pandas as pd
+from sklearn.metrics import r2_score
+from sklearn.model_selection import GridSearchCV # <-- IMPORT THIS
+
+from src.exception import CustomException
+from src.logger import logging
 
 def save_object(file_path, obj):
-    """
-    This function saves a Python object to a file.
-    """
     try:
         dir_path = os.path.dirname(file_path)
         os.makedirs(dir_path, exist_ok=True)
@@ -14,5 +18,44 @@ def save_object(file_path, obj):
             dill.dump(obj, file_obj)
 
     except Exception as e:
-        # You should raise your CustomException here
-        raise e
+        # FIX: Raise the CustomException for consistent error logging
+        raise CustomException(e, sys)
+
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
+    try:
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            para = param[list(models.keys())[i]] # Get parameters for the specific model
+
+            # Use GridSearchCV to find the best parameters
+            gs = GridSearchCV(model, para, cv=3)
+            gs.fit(X_train, y_train)
+
+            # Get the best parameters and set them on the model
+            model.set_params(**gs.best_params_)
+            model.fit(X_train, y_train) # Retrain the model with the best parameters
+
+            # Make predictions
+            y_train_pred = model.predict(X_train)
+            y_test_pred = model.predict(X_test)
+
+            # Calculate R2 scores
+            train_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+        raise CustomException(e, sys)
+
+def load_object(file_path):
+    try:
+        with open(file_path, "rb") as file_obj:
+            return dill.load(file_obj)
+
+    except Exception as e:
+        raise CustomException(e, sys)
